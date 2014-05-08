375ded1af0ff2235be7505ec9fb1e01e5517b638
root <root@vps01.37t.net>
2014-05-08 21:45:11 +0900

    getfeed with thread

--- getfeed.py
+++ getfeed.py
@@ -1,9 +1,10 @@
 #!/data/virt/flask/bin/python
 # -*- encoding: utf-8 -*-
 
-import feedparser, sqlite3
-import time, re
+import feedparser
+import time, re, threading, sqlite3
 
+entries = {}
 
 class db(object):
 
@@ -13,46 +14,57 @@ class db(object):
     self.con = sqlite3.connect( self.file )
     self.cur = self.con.cursor()
 
-
   def select( self, query, args=(), one=False ):
     self.cur.execute( query, args )
     res = self.cur.fetchall()
     return res
 
   def update( self, query,args=(), one=False ):
-    self.cur.execute( query, args )
+    try: ret = self.cur.execute( query, args )
+    except sqlite3.OperationalError: pass
 
   def commit( self ):
     self.con.commit()
+
+
+def getfeed(id, url):
+   
+  fp = feedparser.parse(url)
+  if fp: entries[id] = fp.entries
+
+
+def update_entries( d, id, entries ):
+    if len(entries) < 1: return
+
+    for entry in entries:
+        if entry.has_key("published_parsed"): date = time.mktime(entry.published_parsed) + 32400
+        else: date = time.mktime(entry.updated_parsed) + 32400
+
+        if entry.has_key("content"): summary = entry.content[0].value
+        else: summary = entry.summary
+
+        article = [ id, entry.link, entry.title, date, summary ]
+
+        target = d.select('select id from entries where url = ?',[ entry.link ])
+        if not target and not re.search("^PR|AD",entry.title):
+#          print article
+          d.update('insert into entries values(null,?,?,?,?,?,0,0)',article)
+
+    unread = d.select('select count(read) from entries where feed_id=? and read != 1',[ id ])[0][0]
+#    print unread
+    d.update('update blogs set unread = ? where id = ?',[ unread, id ] )
 
 
 if __name__ == "__main__":
 
     d = db()
 
-    for feeddb in d.select('select id,feed_url from blogs where get = 1'):
+    urls = d.select('select id,feed_url from blogs where get = 1 limit 1')
+    threads = [threading.Thread(target=getfeed, args=(id,url,)) for id, url in urls]
 
-      id = feeddb[0]
-      url = feeddb[1]
+    for thread in threads: thread.start()
+    for thread in threads: thread.join()
 
-      fp = feedparser.parse(url)
+    for key in entries:  update_entries(d, key, entries[key] ) 
 
-      if fp:
-        for entry in fp.entries:
-
-          if entry.has_key("published_parsed"): date = time.mktime(entry.published_parsed) + 32400
-          else: date = time.mktime(entry.updated_parsed) + 32400
-
-          if entry.has_key("content"): summary = entry.content[0].value
-          else: summary = entry.summary
-
-          article = [ id, entry.link, entry.title, date, summary ]
-
-          target = d.select('select id from entries where url = ?',[ entry.link ])
-          if not target and not re.search("^PR|AD",entry.title):
-            d.update('insert into entries values(null,?,?,?,?,?,0,0)',article)
-
-          unread = d.select('select count(read) from entries where feed_id=? and read != 1',[ id ])[0][0]
-          d.update('update blogs set unread = ? where id = ?',[ unread, id ] )
- 
     d.commit()
